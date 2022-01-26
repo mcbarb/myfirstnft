46c187210dfbbe45952fc271047b069872b01ed9
Marcelo Castro Barbosa <celocb@gmail.com>
2022-01-26 07:38:59 -0300

    added metadataFactory and first NFT data and metadata, as well as commented scripts to do them

---
+++ scripts/log.js
@@ -0,0 +1,16 @@
+// NFT #001
+// Error
+
+// Cacau #002
+// manually created JSON
+// mintNFT('https://gateway.pinata.cloud/ipfs/QmVRmzMHtEbupoATSoP4cYbpERvxgjr9yZc1mHLF9m3k5Z')
+
+// Tubára #003
+// createNftMetadata(
+//     3,
+//     "Tubára", 
+//     "Drawing of Tubára, ficticious character that save olives from food poisoning.",
+//     "https://gateway.pinata.cloud/ipfs/QmafJaXiBhr3SeJbqWvM7Ya8YVU9q46ZWucNHvw8qPHZ6g",
+//     {'power': 'green apple', cerro: 'Chapelco', city: 'San Martin de Los Andes', country: 'Argentina', 'year': 2007}
+// )
+// mintNFT('https://gateway.pinata.cloud/ipfs/QmboJkUzSAYEpYgTumZBG9sRyXZ78jjP8GrokxiFkSwWs2')
